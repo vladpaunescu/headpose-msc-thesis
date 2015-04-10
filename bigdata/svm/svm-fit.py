1dafcb989117cdafad002157abfa648f433f79cf
Vlad Păunescu <vlad.paunescu@gmail.com>
2015-04-10 13:28:38 +0300

    Added more dims

--- bigdata/svm/svm-fit.py
+++ bigdata/svm/svm-fit.py
@@ -49,7 +49,6 @@ def predict(xs, ys, t):
     
         plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
      
-    
         # Plot also the training points
         plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
         plt.xlabel('x1')
@@ -69,7 +68,7 @@ def perdictData(X, y, S):
     # data since we want to plot the support vectors
     C = 1.0  # SVM regularization parameter
     svc = svm.SVC(kernel='linear', C=C).fit(X, y)
-    rbf_svc = svm.SVC(kernel='rbf', probability=True, gamma=100, C=C).fit(X, y)
+    rbf_svc = svm.SVC(kernel='rbf', probability=True, gamma=1, C=C).fit(X, y)
     poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
     lin_svc = svm.LinearSVC(C=C).fit(X, y)
 
@@ -116,7 +115,6 @@ def perdictData(X, y, S):
         plt.title(titles[i])
 
     plt.show()
-    
 
 def twoMoons(n, dist, radius):
     xs = np.zeros(n)
@@ -136,28 +134,27 @@ def twoMoons(n, dist, radius):
     return (xs, ys, ts)
 
 def predictMultidim(X, y, S):
-    C = 1.0  # SVM regularization parameter
-    rbf_svc = svm.SVC(kernel='rbf', probability=True, gamma=1, C=C).fit(X, y)
+    C = 10.0  # SVM regularization parameter
+    rbf_svc = svm.SVC(kernel='rbf', gamma=1.5, C=C).fit(X, y)
     
     Z = rbf_svc.predict(S)
+    print Z.shape, " shapes ", S.shape
     print set(Z)
     count1, count2 = 0, 0
     for i, el in enumerate(Z):
         if el == 1: 
             count1 +=1
             if count1 < 100:
-                print S[i] 
+                print S[i]
         else:
             count2 += 1
     print "Counts: {} {}".format(count1, count2)
+    return (S, Z)
 
 def generateDataset(pos, neg, unk, n, p):
-    upCount, downCount = 2, 0
+    upCount, downCount = 1, 1
     ups = [(np.random.rand(p), np.ones(pos))  for _ in range(upCount)]
-    
-    print ups[0][0]
-    print ups[1][0]
-    
+#   
     downs = [(np.random.rand(p), np.zeros(neg))  for _ in range(downCount)]
     t4 = np.ones(unk) * 2
     X = None
@@ -165,14 +162,14 @@ def generateDataset(pos, neg, unk, n, p):
     for up in ups:
         if X is not None:
             y = np.concatenate((y, up[1]))
-            X = np.concatenate((X, np.array([up[0] for _ in range(pos)])))
+            X = np.concatenate((X, np.array([up[0] + np.random.rand(p) / 10 for _ in range(pos)])))
         else:
             y = np.array(up[1])
-            X = np.array([up[0] for _ in range(pos)])
+            X = np.array([up[0] + np.random.rand(p) / 10 for _ in range(pos)])
       
     for down in downs:
         y = np.concatenate((y, down[1]))
-        X = np.concatenate((X, np.array([down[0]  for _ in range(neg)])))
+        X = np.concatenate((X, np.array([down[0] + np.random.rand(p) / 10 for _ in range(neg)])))
         
     X4 = np.random.rand(unk, p)
 
@@ -180,18 +177,65 @@ def generateDataset(pos, neg, unk, n, p):
     y = np.concatenate((y, t4))
     S = np.random.rand(n, p)
     return (X, y, S)
-    
+
+def plotSubspace(X, y, S, Z):
+    plt.subplots_adjust(wspace=0.7, hspace=0.7)
+    m, n = X.shape
+    dimSet = set()
+    dims = ()
+    for i in xrange(16):
+        plt.subplot(4, 4, i + 1)
+        while True:
+            dims = tuple(np.random.randint(0, n, 2).tolist())
+            if dims[0] == dims[1]: 
+                continue
+            if dims in dimSet:
+                continue
+            dimSet.add(dims)
+            break
+        
+        print dims, S[:, dims[1]].shape, Z.shape
+        
+        print S[:, dims[0]]
+        print S[:, dims[1]]
+        print "Y = ", y
+        print [int(el) for el in Z]
+        xx = []
+        t = []
+        for (i, el) in enumerate(Z):
+            if el != 2.0:
+                xx.append(S[i])
+                t.append(int(el))
+        xx = np.array(xx)
+        print "Size ", xx.size
+        if xx.size > 0:
+            plt.scatter(xx[:, dims[0]], xx[:, dims[1]], c=t, cmap=plt.cm.Paired)
+        else:
+            pass
+        
+        xx = []
+        t = []
+        for (i, el) in enumerate(y):
+            if el != 2.0:
+                xx.append(X[i])
+                t.append(int(el))
+        xx = np.array(xx)
+        plt.scatter(xx[:, dims[0]], xx[:, dims[1]], c=t, s=30, alpha=0.6, cmap=plt.cm.Paired)
+         
+        plt.xlabel('x{}'.format(dims[0]))
+        plt.ylabel('x{}'.format(dims[1]))
+        plt.xlim(S[:, dims[0]].min(), S[:, dims[0]].max())
+        plt.ylim(S[:, dims[1]].min(), S[:, dims[1]].max())
+        plt.xticks(())
+        plt.yticks(())
+    plt.show()
 
 if __name__ == "__main__":
     n, dist, radius = 40, 7, 5
 #     (xs, ys, ts) = twoMoons(n, dist, radius)
 #     plotScatter(xs, ys, ts)
 #     predict(xs, ys, ts)
-    pos, neg, unk, n, p = 10, 10, 100, 800000, 10
+    pos, neg, unk, n, p = 2000, 2000, 10000, 10000, 15
     (X, y, S) = generateDataset(pos, neg, unk, n, p)
-    predictMultidim(X, y, S)
-    
-    
-    
-    
-    
+    S, Z = predictMultidim(X, y, S)
+    plotSubspace(X, y, S, Z)
